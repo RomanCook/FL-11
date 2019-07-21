27fd01ba0117e02bcf9450238fcc198f66888144
Roman Kuk <34649365+RomanCook@users.noreply.github.com>
2019-07-21 21:47:30 +0300

    Update fighters_game.js

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -7,49 +7,48 @@ function Fighter(name, damage, hp, agility, combatWins, combatLosses){
         Wins: combatWins,
         Losses: combatLosses
     },
-    this.getName = function(){
-            console.log(this.name);
-        },
-        this.getDamage = function(){
-            let damage = this.damage;
-            console.log(damage);
-        },
-        this.getAgility = function(){
-            let agility = this.agility;
-            console.log(agility);
-        },
-        this.getHelth = function(){
-            let helth = this.hp;
-            console.log(helth);
-        },
-        this.attack = function(defender){
-            let sto = 100;
-            if(Math.ceil(Math.random() * sto) > defender.hp){
-                defender.hp -= this.damage;
-            } else {
-                console.log(`${defender.name} attak missed`);
-            }
-        },
-        this.logCombatHistory = function(){
-            console.log(`Name: ${this.getName}, ${this.combatHistory}`)
-        },
-        this.heal = function(a){
-            let sto = 100;
-            if(this.hp + a > sto){
-                this.hp += a;
-            }
-        },
-        this.dealDamage = function(b){
-            if(this.hp - b >= 0) {
-                this.hp -= b;
-            } else {
-                this.hp = 0;
-            }
-        },
-        this.addWin = function(){
-            this.Wins ++;
-        },
-        this.addLoss = function(){
-            this.Losses ++;
+    Fighter.prototype.getName = function(){
+      console.log(this.name);
+    },
+    Fighter.prototype.getDamage = function(){
+      console.log(this.damage);
+    },
+    Fighter.prototype.getAgility = function(){
+      let agility = this.agility;
+      console.log(agility);
+    },
+    Fighter.prototype.getHelth = function(){
+      let helth = this.hp;
+      console.log(helth);
+    },
+    Fighter.prototype.attack = function(defender){
+      let sto = 100;
+      if(Math.ceil(Math.random() * sto) > defender.hp ){
+        defender.hp -= this.damage;
+      } else {
+        console.log(`${defender.name} attak missed`);
+      }
+    },
+    Fighter.prototype.logCombatHistory = function(){
+      console.log(`Name: ${this.getName}, ${this.combatHistory}`)
+    },
+    Fighter.prototype.heal = function(a){
+      let sto = 100;
+      if(this.hp + a > sto){
+        this.hp += a;
+      }
+    },
+    Fighter.prototype.dealDamage = function(b){
+        if(this.hp - b >= 0) {
+            this.hp -= b;
+        } else {
+            this.hp = 0;
         }
+    },
+    Fighter.prototype.addWin = function(){
+        this.Wins ++;
+    },
+    Fighter.prototype.addLoss = function(){
+        this.Losses ++;
     }
+}
